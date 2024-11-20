d0661a2e7cf3fa0f22980be8a446ebf18a68e8b6
AshilaMahdiyyah <ashilamahdiyyah03@gmail.com>
2024-11-20 14:21:40 +0700

    welcome_screen

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,78 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:tugas3/screens/home_screen.dart';
 
-class WelcomeScreen extends StatelessWidget{
+class WelcomeScreen extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("images/bg.jpg"),
-          fit: BoxFit.cover,
-          opacity: 0.7,
-        )
-      ),
-      child: Material(
-        color: Colors.transparent,
-        child: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(vertical: 65, horizontal: 25),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Enjoy", 
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 35,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 1.5,
-                  ),
-                ),
-                SizedBox(height: 2),
-                Text(
-                  "the world!", 
-                  style: TextStyle(
-                    color: Colors.white.withOpacity(0.9),
-                    fontSize: 35,
-                    fontWeight: FontWeight.w400,
-                    letterSpacing: 1.5,
-                  ),
-                ),
-                SizedBox(height: 12),
-                Text(
-                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-                  style: TextStyle(
-                    color: Colors.white.withOpacity(0.7),
-                    fontSize: 16,
-                    letterSpacing: 1.2,
-                  ),
-                ),
-                SizedBox(height: 30),
-                InkWell(
-                  onTap: (){
-                    Navigator.push(context, MaterialPageRoute(
-                      builder: (context) => HomePage(),
-                    ));
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/EJGO.png"),
+            fit: BoxFit.cover,
+            opacity: 0.7,
+          ),
+        ),
+        child: Stack(
+          children: [
+            // Konten lain dapat ditambahkan di sini jika diperlukan
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => HomePage(),
+                      ),
+                    );
                   },
-                  child: Ink(
-                    padding: EdgeInsets.all(15),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 15, horizontal: 50),
                     decoration: BoxDecoration(
                       color: Colors.white,
                       borderRadius: BorderRadius.circular(12),
                     ),
-                    child: Icon(
-                      Icons.arrow_forward_ios,
-                      color: Colors.black54,
-                      size: 20,
+                    child: Text(
+                      'Start',
+                      style: TextStyle(
+                        color: Colors.black54,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ),
-              ],
+              ),
             ),
-          ),
+          ],
         ),
-      ),  
+      ),
     );
   }
 }
